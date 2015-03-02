8d70d436740d78972475390ada6af1a86864d124
Jmz <jmztaylor@gmail.com>
2015-03-02 20:07:07 +0100

    Force Expanded Notifications (1/2)
    This will force expanded notifications for all apps that support
    expanded notifications
    
    Change-Id: I1f6fac658a3e58488bc37cafec9208a75da78ab8
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3497,6 +3497,12 @@ public final class Settings {
          * @hide
          */
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
+        
+        /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
